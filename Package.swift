6d25fdddd863ad9ef67aa1aba4c68b54e719565b
jerometonnelier <jerometonnelier@yahoo.fr>
2021-08-12 10:35:53 +0200

    KAIMAN-iOS

--- Package.swift
+++ Package.swift
@@ -16,16 +16,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/jerometonnelier/KExtensions", .branch("master")),
-        .package(url: "https://github.com/jerometonnelier/ATAViews", .branch("master")),
-        .package(url: "https://github.com/jerometonnelier/ATAConfiguration", .branch("master")),
-        .package(url: "https://github.com/jerometonnelier/ActionButton", .branch("master")),
-        .package(url: "https://github.com/jerometonnelier/KCoordinatorKit", .branch("master")),
+        .package(url: "https://github.com/KAIMAN-iOS/KExtensions", .branch("master")),
+        .package(url: "https://github.com/KAIMAN-iOS/ATAViews", .branch("master")),
+        .package(url: "https://github.com/KAIMAN-iOS/ATAConfiguration", .branch("master")),
+        .package(url: "https://github.com/KAIMAN-iOS/ActionButton", .branch("master")),
+        .package(url: "https://github.com/KAIMAN-iOS/KCoordinatorKit", .branch("master")),
         .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.0"),
-        .package(url: "https://github.com/jerometonnelier/TextFieldEffects", .branch("master")),
+        .package(url: "https://github.com/KAIMAN-iOS/TextFieldEffects", .branch("master")),
         .package(url: "https://github.com/gordontucker/FittedSheets", from: "2.0.0"),
         .package(url: "https://github.com/mxcl/PromiseKit", from: "6.13.2"),
-        .package(url: "https://github.com/jerometonnelier/KStorage", .branch("master")),
+        .package(url: "https://github.com/KAIMAN-iOS/KStorage", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
